a271249a8321839f18ad63261dafd88fe5994238
ItsMe <231827038+Its-Me573@users.noreply.github.com>
2025-10-27 16:30:36 -0700

    parse string thing

--- main.cpp
+++ main.cpp
@@ -53,32 +53,31 @@ string outfileName = "updatedPlayer.txt";
 string player_inventory_input = " ";
 cout << "Please load your character data." << endl;
 cin >> player_inventory_input;
-ifstream playerInventory;
-playerInventory.open(player_inventory_input);
+playerInfo.open(player_inventory_input);
 
 
-if(playerInventory.is_open()){
+if(playerInfo.is_open()){
     string temporaryString = " ";
     //--------------------------------------------------------------------------
     //runs if the correct file was opened. 
     //read the first line with the coordinates of the player
     //parse the string to extract player X and Z and move to those variables
-    getline(playerInventory, temporaryString);
+    getline(playerInfo, temporaryString);
     playerX = stoi(temporaryString.substr(0, temporaryString.find(' ')));
     playerZ = stoi(temporaryString.substr(temporaryString.find(' ') + 1));
-    cout << playerX << endl;
-    cout << playerZ << endl;
+    //cout << playerX << endl;
+    //cout << playerZ << endl;
     //--------------------------------------------------------------------------
     //read the second line of the file
     //1 or 0 will dertime axe true or not
-    getline(playerInventory, temporaryString);
+    getline(playerInfo, temporaryString);
     if(temporaryString == "0")
         hasAxe = false;
     else
         hasAxe = true;
     //--------------------------------------------------------------------------
     //reading the third row, amount of carrots to defeat shulker, apparently
-    getline(playerInventory, temporaryString);
+    getline(playerInfo, temporaryString);
     carrotsNeeded = stoi(temporaryString);
     //--------------------------------------------------------------------------
     //reading all remaining rows in the format (itemID itemAmount)
@@ -87,7 +86,7 @@ if(playerInventory.is_open()){
     int currentRow = 0;
     int currentColumn = 0;
 
-    while(getline(playerInventory, temporaryString)){
+    while(getline(playerInfo, temporaryString)){
         currentItem = temporaryString[0];
         //cout << "Current item is " << currentItem << endl;
         itemCount = stoi(temporaryString.substr(temporaryString.find(' ') + 1));
@@ -128,10 +127,87 @@ if(playerInventory.is_open()){
 //outer loop for the rows
 for(int i = 0; i < 3; i++){
     for(int k = 0; k < 9; k++){
-        cout << itemList[i][k] << " " << itemAmount[i][k] << endl;
+        //cout << itemList[i][k] << " " << itemAmount[i][k] << endl;
     }
 }
-cout << "The total carrots are " << totalCarrots << endl;
-cout << "The total rockets are " << totalRockets << endl;
+//cout << "The total carrots are " << totalCarrots << endl;
+//cout << "The total rockets are " << totalRockets << endl;
+
+//Part 2----------------------------------------------------------------------
+//reading and parsing through a string test
+//"T,4,S,12,R,5,T,8,S,3"
+string readingTest = "T,2.5,S,1.5,R,3.0"; //12 trees, 15 shulkers, 5 rockets //current error, it is reading each individual index
+//fix, current integer as a string and append integer string until a comma is scanned
+int tempTreeAmount = 0;
+int tempShulkerAmount = 0;
+int tempRocketsNeeded = 0;
+int currentCommaIndex = readingTest.find(',');
+bool isTree = false;
+bool isShulker = false;
+bool isRocket = false;
+string currentInt = "";
+for(int i = 0; i < readingTest.length(); i++){
+    string currentCharacter = "";
+    currentCharacter = readingTest[i];
+    
+    if(currentCharacter == ","){
+        if(currentInt != "" && isTree == true){
+            tempTreeAmount += stoi(currentInt);
+            isTree = false;
+            currentInt = "";
+        }
+        if(currentInt != "" && isShulker == true){
+            tempShulkerAmount += stoi(currentInt);
+            isShulker = false;
+            currentInt = "";
+        }
+        if(currentInt != "" && isRocket == true){
+            tempRocketsNeeded += stoi(currentInt);
+            isRocket = false;
+            currentInt = "";
+        }
+        continue;
+    }
+
+    if(currentCharacter == "T"){
+        isTree = true;
+        continue;
+    }   
+    if(currentCharacter == "S"){
+        isShulker = true;
+        continue;
+    }
+    if(currentCharacter == "R"){
+        isRocket = true;
+        continue;
+    }
+
+    currentInt = currentInt + currentCharacter;
+
+     if(i == readingTest.length()-1){
+        if(isTree == true){
+            tempTreeAmount += stoi(currentInt);
+            isTree = false;
+            currentInt = "";
+        }
+        if(isShulker == true){
+            tempShulkerAmount += stoi(currentInt);
+            isShulker = false;
+            currentInt = "";
+        }
+        if(isRocket == true){
+            tempRocketsNeeded += stoi(currentInt);
+            isRocket = false;
+            currentInt = "";
+        }
+     }
+
+
+
+}
+cout << endl;
+cout << "Trees in the way is: " << tempTreeAmount << endl;
+cout << "Shulkers amount is: " << tempShulkerAmount << endl;
+cout << "Rockets needed is:" << tempRocketsNeeded << endl;
 return 0;
 }
